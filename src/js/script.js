52be5aa75c128b6a76a94b961ab81ff684ccb811
Suren <suren.khachatryan1999@bk.ru>
2021-03-06 17:32:25 +0300

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
         slidesToShow: 1,
         speed:2500,
         infinite:true,
-        autoplay:true,
+        autoplay:false,
         autoplaySpeed: 5000,
         leftMode: true,
-        variableWidth:true
+        variableWidth:true,
+        responsive: [
+            {
+                breakpoint:768,
+                settings: {
+                    autoplaySpeed:4000
+                }
+            }
+        ]
     });
 });
